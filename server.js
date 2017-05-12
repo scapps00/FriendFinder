39ac0d5cc415ebd7208d24c2ff0dcce9453d6498
Samantha Capps <samantha@Cheez.local>
2017-05-11 22:48:07 -0400

    comparisons

--- server.js
+++ server.js
@@ -23,12 +23,29 @@ app.post("/api/add", function(req, res) {
 	fs.readFile(path.join(__dirname, "/app/data/friends.js"), (err, data) => {
 		if (err) throw err;
 		var friends = JSON.parse(data);
+		var match = friends[compare(friends, newPerson)].name;
+		res.send(match);
 		friends.push(newPerson);
 		fs.writeFile(path.join(__dirname, "/app/data/friends.js"), JSON.stringify(friends), (err) => {
 			if (err) throw err;
 		});
 	});	
 });
+
+function compare(oldGuys, newGuy) {
+	var differenceArray = [];
+	for (var i = 0; i < oldGuys.length; i++) {
+		var totalDifference = 0;
+		for (var j = 0; j<10; j++) {
+			var difference = Math.abs(oldGuys[i].answers[j] - newGuy.answers[j]);
+			totalDifference += difference;
+		}
+		differenceArray.push(totalDifference);
+	}
+	var closestMatch = Math.max.apply(null, differenceArray);
+	var index = differenceArray.indexOf(closestMatch);
+	return index;
+}
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
