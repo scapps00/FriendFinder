4a1b9d211cf3087bd0025d83a95269102a52108c
Samantha Capps <samantha@Cheez.local>
2017-05-15 14:35:28 -0400

    done I think

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -24,4 +24,8 @@ module.exports = function(app) {
 	app.get("/friendList", function(req, res) {
 		res.sendFile(path.join(__dirname, "../data/friends.js"));
 	});
+
+	app.use(function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/home.html"));
+	});
 }
