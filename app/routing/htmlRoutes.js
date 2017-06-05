680aece53be6f32be104a482f455978f7dc6b967
Samantha Capps <samantha@wireless-v47-press00130.dhcp.unc.edu>
2017-06-05 19:05:17 -0400

    weird thing fixed

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -25,7 +25,7 @@ module.exports = function(app) {
 		res.sendFile(path.join(__dirname, "../data/friends.js"));
 	});
 
-	app.use(function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/home.html"));
-	});
+	// app.use(function(req, res) {
+	// 	res.sendFile(path.join(__dirname, "../public/home.html"));
+	// });
 }
